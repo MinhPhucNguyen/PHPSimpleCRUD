574123047c57155a200b7abe0b17ca52caad4f07
MinhPhucNguyen <2051063667@e.tlu.edu.vn>
2023-03-25 19:16:49 +0700

    Up view.php and index.php

--- index.php
+++ index.php
@@ -1,18 +1,42 @@
 <?php
-    require 'users.php';
-    getUsers();
-?>
+require 'users.php';
+$users = getUsers();
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple CRUD</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-<body>
-    
-</body>
-</html>
+include 'partials/header.php';
+?>
+<div class="container">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Website</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($users as $user) : ?>
+                <tr>
+                    <td><?php echo $user['name'] ?></td>
+                    <td><?php echo $user['username'] ?></td>
+                    <td><?php echo $user['email'] ?></td>
+                    <td><?php echo $user['phone'] ?></td>
+                    <td>
+                        <a target="_blank" href="http://<?php echo $user['website']?>">
+                            <?php echo $user['website'] ?>
+                        </a>
+                    <td>
+                        <a href="view.php?id=<?php echo $user['id'] ?>" class="btn btn-sm btn-outline-info">View</a>
+                        <a href="update.php?id=<?php echo $user['id'] ?>" class="btn btn-sm btn-outline-secondary">Update</a>
+                        <a href="delete.php?id=<?php echo $user['id'] ?>" class="btn btn-sm btn-outline-danger">Delete</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
+
+
+<?php include 'partials/footer.php' ?>
--- users.php
+++ users.php
@@ -1,12 +1,20 @@
 <?php
 function getUsers()
 {
-    $users = json_decode(file_get_contents(__DIR__.'/users.json'), true);
+    //associative array: sử dụng các khóa được đặt tên và gắn giá trị 
+    return json_decode(file_get_contents(__DIR__.'/users.json'), true);
+
 }
 
 function getUserByID($id)
 {
-
+    $users = getUsers();
+    foreach($users as $user) {
+        if($user['id'] == $id ){
+            return $user;
+        }
+    }
+    return null;
 }
 
 function createUser($data)
